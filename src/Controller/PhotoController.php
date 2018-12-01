cd1ee333ae94ca9dbeaef7c4930bf8022e1de9f5
Mihail Nikolov <micobg@gmail.com>
2018-12-01 22:46:59 +0200

    Empty array fix

--- src/Controller/PhotoController.php
+++ src/Controller/PhotoController.php
@@ -96,16 +96,18 @@ class PhotoController extends AbstractController
     {
         $result = [];
 
-        foreach ($metadata['sectors'] as $k => $v) {
-            $v['geo'] = json_decode($v['geo'], true);
-            $v['position'] = [
-                'top' => $v['geo']['coordinates'][0][0][1],
-                'left' => $v['geo']['coordinates'][0][0][0],
-                'width' => $v['geo']['coordinates'][0][2][0] - $v['geo']['coordinates'][0][0][0],
-                'height' => $v['geo']['coordinates'][0][2][1] - $v['geo']['coordinates'][0][0][1],
-            ];
+        if (!empty($metadata['sectors'])) {
+            foreach ($metadata['sectors'] as $k => $v) {
+                $v['geo'] = json_decode($v['geo'], true);
+                $v['position'] = [
+                    'top' => $v['geo']['coordinates'][0][0][1],
+                    'left' => $v['geo']['coordinates'][0][0][0],
+                    'width' => $v['geo']['coordinates'][0][2][0] - $v['geo']['coordinates'][0][0][0],
+                    'height' => $v['geo']['coordinates'][0][2][1] - $v['geo']['coordinates'][0][0][1],
+                ];
 
-            $result[] = $v;
+                $result[] = $v;
+            }
         }
 
         return $result;
