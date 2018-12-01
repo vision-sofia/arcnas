e0bae675ef2d1db96300313efcaec3e0d1271a1f
Mihail Nikolov <micobg@gmail.com>
2018-12-01 23:53:36 +0200

    Login form and colors

--- assets/plugins/sel/app.js
+++ assets/plugins/sel/app.js
@@ -3,3 +3,10 @@ var activeRectCoordinates = null;
 window.onload = function () {
   // rectCtrl.drawPassiveRects(passiveRects);
 };
+
+
+var loginCtrl = {
+  login: function () {
+    $('.login-form').submit();
+  }
+};
