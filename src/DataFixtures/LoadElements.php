2e4435c1a671f92056092e35b52b5a476c7c9cde
yorndv <yordan@ednodarvo.io>
2020-03-25 22:04:30 +0200

    Update data fixtures

--- src/DataFixtures/LoadElements.php
+++ src/DataFixtures/LoadElements.php
@@ -4,7 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\ConfigurationList\Element;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 
 class LoadElements extends Fixture
 {
--- src/DataFixtures/LoadUsers.php
+++ src/DataFixtures/LoadUsers.php
@@ -4,18 +4,23 @@ namespace App\DataFixtures;
 
 use App\Entity\User\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class LoadUsers extends Fixture
 {
+    private UserPasswordEncoderInterface $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        # username: demo
-        # password: demo
-
         $object = new User();
         $object->setUsername('demo');
-        $object->setPassword('$argon2i$v=19$m=1024,t=2,p=2$c29tZXNhbHQ$ulX0wpeE7xPhD0p6pOy3VxT4TPlnaIDYdHwYMGGedYs');
+        $object->setPassword($this->passwordEncoder->encodePassword($object, 'demo'));
         $object->addRole('ROLE_USER');
         $object->setIsActive(true);
 
